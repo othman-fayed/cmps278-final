0879fc46f53939aeee90e31a8bdecd973dc88475
Othman Fayed <o.fayed@prfd.aero>
2022-12-14 23:02:12 +0200

    Prior to Identity scoffold

--- DocumentsWeb/Program.cs
+++ DocumentsWeb/Program.cs
@@ -1,38 +1,46 @@
 using DocumentsWeb.Areas.Identity;
 using DocumentsWeb.Data;
+using DocumentsWeb.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+	?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+	options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services 
+	.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+	.AddEntityFrameworkStores<ApplicationDbContext>()
+	;
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
 builder.Services.AddSingleton<WeatherForecastService>();
+// Services
+builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
+	app.UseMigrationsEndPoint();
 }
 else
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
---
+++ DocumentsWeb/Services/EmailSender.cs
@@ -0,0 +1,33 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
+
+namespace DocumentsWeb.Services
+{
+	public class EmailSender : IEmailSender
+	{
+		public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+		{
+			var message = new MimeMessage();
+			message.From.Add(new MailboxAddress("CMPS278", "omf02@aub.edu.lb"));
+			message.To.Add(new MailboxAddress("Mrs. Chanandler Bong", email));
+			message.Subject = subject;
+
+			message.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+			{
+				Text = htmlMessage
+			};
+
+			using (var client = new SmtpClient())
+			{
+				await client.ConnectAsync("smtp-relay.sendinblue.com", 587, false);
+
+				// Note: only needed if the SMTP server requires authentication
+				await client.AuthenticateAsync("omf02@aub.edu.lb", "9c4pGmvJ7yh1UXWI");
+
+				await client.SendAsync(message);
+				await client.DisconnectAsync(true);
+			}
+		}
+	}
+}
