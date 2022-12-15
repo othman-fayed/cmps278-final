7bb6fbca8c9ffdedf094ff89aafbb21f3ce8886f
Othman Fayed <o.fayed@esdm-flightsupport.com>
2022-12-15 14:56:32 +0200

    WIP - Add User

--- DocumentsWeb/Program.cs
+++ DocumentsWeb/Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services 
 	.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+	.AddRoles<IdentityRole>()
 	.AddEntityFrameworkStores<ApplicationDbContext>()
 	;
 builder.Services.AddRazorPages();
